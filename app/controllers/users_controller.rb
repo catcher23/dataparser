ada5c5adfcf4a409dd9587c4540ec42f398a9033
Danny Lau <danlau168@gmail.com>
2016-07-02 23:19:36 -0700

    save users to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     field_mapping = params['fieldMapping']
     rows_with_no_errors = []
     rows_with_errors = []
+
     data.each_with_index do |d,i|
       # replaces mapped keys
       datum = data[i.to_s].map {|k, v| [field_mapping[k], v] }.to_h
@@ -13,6 +14,14 @@ class UsersController < ApplicationController
         user = User.new(datum)
         validated_row, has_errors = User.validate_user_data(user, datum)
         has_errors ? rows_with_errors << validated_row : rows_with_no_errors << validated_row
+
+        begin
+          user.save
+        rescue ActiveRecord::RecordNotUnique => e
+          next if(e.message =~ /unique.*constraint.*index_users_on_first_name_and_last_name_and_address/)
+          raise
+        end
+
       end
     end
 
